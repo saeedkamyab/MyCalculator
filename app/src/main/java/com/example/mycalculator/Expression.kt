2bf3cfa09b5422fcc36d1e7152f5427c5439a99b
saeedkamyab <saeed.kamyab@yahoo.com>
2024-10-02 09:55:45 +0330

    Expression class has been added

---
+++ app/src/main/java/com/example/mycalculator/Expression.kt
@@ -0,0 +1,11 @@
+package com.example.mycalculator
+
+class Expression(var infixExpression: MutableList<String>) {
+    private fun infixToPostfix(): String {
+
+    }
+
+    fun evaluateExpression(): Double {
+
+    }
+}
