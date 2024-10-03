0e9f9f322dbff3cc7012261b343560fba0257d48
saeedkamyab <saeed.kamyab@yahoo.com>
2024-10-03 10:21:13 +0330

    vide 4 and working on expression

--- app/src/main/java/com/example/mycalculator/Expression.kt
+++ app/src/main/java/com/example/mycalculator/Expression.kt
@@ -1,8 +1,39 @@
 package com.example.mycalculator
 
+import java.util.Stack
+
 class Expression(var infixExpression: MutableList<String>) {
     private fun infixToPostfix(): String {
+        var result = ""
+        val stack = Stack<String>()
+        for (element in infixExpression) {
+            if (element.all { it.isDigit() } || element.any { it == '.' }) {
+                result += "$element"
+            } else if (element == "(") {
+                stack.push(element)
+            } else if (element == ")") {
+                while (stack.peek() != "(" && stack.isNotEmpty()) {
+                    result += "${stack.pop()}"
+                }if (stack.isNotEmpty())
+                    stack.pop()
+            } else {
+                while (stack.isNotEmpty() && precedence(stack.peek()) >= precedence(element)) {
+                    result += "${stack.pop()} "
+                }
+            }
+        }
+        while (stack.isNotEmpty()){
+            result+="${stack.pop()}"
+        }
+        return  result
+    }
 
+    private fun precedence(operator: String): Int {
+        return when (operator) {
+            "*", "/" -> 2
+            "+", "-" -> 1
+            else -> -1
+        }
     }
 
     fun evaluateExpression(): Double {
